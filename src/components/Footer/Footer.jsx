4abb945766c27fec06b0023792a977a928be9188
terravidhal <infiniterraetincelle@gmail.com>
2024-04-25 17:58:17 +0100

    40%

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,34 @@ const Footer = () => {
 
   return(
     <div className="Footer">
-      Footer Component
+      <div className="footer-top">
+        <div className="footer-title">
+          <p>
+            Recommended by Over 40,000 verified customers
+          </p>
+        </div>
+        <div className="footer-items">
+           <div className="footer-item one">
+             <div className="item-title">'Great'</div>
+             <div className="item-stars"> <img src="../src/assets/stars.svg" alt="" /></div>
+             <div className="item-description">Based on 68.513 reviews</div>
+             <div className="item-trust"> <img src="../src/assets/trustpilot.svg" alt="" /></div>
+           </div>
+           <div className="footer-item two">
+            <div className="items-s-m-t">
+              <div className="item-stars"> <img src="../src/assets/stars.svg" alt="" /></div>
+              <div className="item-Stitle">I'd like to have a French communication</div>
+              <div className="item-Mtitle">i'd like to have a French communication</div>
+            </div>
+              <div className="items-name-time">
+                <div className="item-name">Johanne Fréchette</div>
+                <div className="item-time">22 hours, 47 minutes ago</div>
+              </div>
+           </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+      </div>
      </div>
   );
 };
